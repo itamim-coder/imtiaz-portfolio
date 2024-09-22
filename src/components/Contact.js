30fa4c1aa6bed0b7a65c1924c5b0473550b2a123
itamim-coder <itamim12202@gmail.com>
2024-09-23 00:11:56 +0600

    [ADDED] font in contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -35,7 +35,7 @@ const Contact = () => {
       whileInView="animate"
     >
       <motion.div className="textContainer" variants={variants}>
-        <motion.h1 variants={variants}>Let’s work together</motion.h1>
+        <motion.h1 className="title" variants={variants}>Let’s work together</motion.h1>
         <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
           <span>itamim12202@gmail.com</span>
